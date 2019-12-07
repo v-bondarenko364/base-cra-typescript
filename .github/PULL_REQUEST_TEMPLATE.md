86afc8106f0842ba3f622ed24a116703a2fdc3db
Vadim <vadim.b@develops.today>
2019-12-07 18:01:00 +0200

    feat(routing): add basic routing and pr template

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,18 @@
+## PR Name: **PR_NAME**
+
+### Trello Task: [**TASK_TITLE**](__TASK_LINK__)
+
+### In this PR:
+
+> Enter any useful information about this pull request.
+
+---
+
+### QA instructions:
+
+> Please specify if there's any information that can be helpful during the QA process.
+
+### Checklist
+
+- [ ] Your PR depends on others PR's
+- [ ] Include updating API endpoint (need provide updated postman collection)
--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,39 @@ import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles';
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import RepositoryList from '@modules/components/RepositoryList';
+import Home from '@modules/Home/pages/Home';
+import RepositoryList from '@modules/Repository/pages/RepositoryList';
+import NotFound from '@modules/Home/pages/NotFound';
 import { GlobalStyle, theme } from '@theme/themes/base';
 import muiTheme from '@theme/themes/muiTheme';
 import store from '@store/index';
 import useI18n from '@services/i18n';
+// import WithRouteParams from './hoks/WithRouteParams';
 
 const App: React.FC = () => {
 	const i18n = useI18n('ru');
 
 	return (
 		<Provider store={store}>
-			<MuiThemeProvider theme={muiTheme}>
-				<ThemeProvider theme={theme}>
-					<>
+			<BrowserRouter>
+				<MuiThemeProvider theme={muiTheme}>
+					<ThemeProvider theme={theme}>
 						<GlobalStyle />
-						<RepositoryList i18n={i18n} />
-					</>
-				</ThemeProvider>
-			</MuiThemeProvider>
+						<Switch>
+							<Route exact path="/" render={props => <Home {...props} i18n={i18n} />} />
+							<Route
+								exact
+								path="/list"
+								render={props => <RepositoryList {...props} i18n={i18n} />}
+							/>
+							<Route component={NotFound} />
+						</Switch>
+					</ThemeProvider>
+				</MuiThemeProvider>
+			</BrowserRouter>
 		</Provider>
 	);
 };
