5eef32d4a3a361e9d1d8ff05ed59323a882130f5
Vadim <vadim.b@develops.today>
2019-12-23 13:48:53 +0200

    feat( test ): add error

---
+++ src/error.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const error = () => {
+	throw new Error('test');
+};
+export default error;
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,19 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import SentryConfig from './config/sentry';
 
+import Error from './error';
+
 // if (process.env.NODE_ENV !== Environment.Development) {
 Sentry.init(SentryConfig);
 // }
 // console.log(process.env);
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<p>
+		<Error />
+		<App />
+	</p>,
+	document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
